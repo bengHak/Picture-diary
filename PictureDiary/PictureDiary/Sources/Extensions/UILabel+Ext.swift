de9ab279fcf1e8234fb8d45f994d951806b414e7
bengHak <qkwl4678@naver.com>
2022-03-06 17:32:51 +0900

    feat: Add UILabel text center alignment

---
+++ PictureDiary/PictureDiary/Sources/Extensions/UILabel+Ext.swift
@@ -0,0 +1,29 @@
+//
+//  UILabel+Ext.swift
+//  PictureDiary
+//
+//  Created by byunghak on 2022/03/05.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    func setTextWithLineHeight(text: String?, fontSize: CGFloat, fontWeight: UIFont.Weight, lineHeight: CGFloat) {
+        if let text = text {
+            let style = NSMutableParagraphStyle()
+            style.maximumLineHeight = lineHeight
+            style.minimumLineHeight = lineHeight
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .paragraphStyle: style,
+                .font : UIFont.systemFont(ofSize: fontSize, weight: fontWeight),
+                .baselineOffset: (lineHeight - font.lineHeight) / 2 + font.descender
+            ]
+                
+            let attrString = NSAttributedString(string: text,
+                                                attributes: attributes)
+            self.attributedText = attrString
+        }
+    }
+}
